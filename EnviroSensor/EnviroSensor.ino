908b02f1f19fb66918d192887e6eb6361b303e86
GrayOrion <grayorionable@gmail.com>
2023-07-10 14:36:33 -0700

    Added WiFi Manager
    * Fixed issues with LED
    * Added WiFi manager code - if no wifi info provided in files, will create a hub and a webform to intake info, writing all info into the \data\* files.

--- EnviroSensor/EnviroSensor.ino
+++ EnviroSensor/EnviroSensor.ino
@@ -12,7 +12,35 @@
 #include <Adafruit_BME280.h> // specific code to access the BME280
 
 
-// Constants for the OLED Screen
+
+// From WifiManager
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncTCP.h>
+#include "SPIFFS.h"
+
+// Search for parameter in HTTP POST request
+const char* PARAM_INPUT_1 = "ssid";
+const char* PARAM_INPUT_2 = "pass";
+const char* PARAM_INPUT_3 = "ip";
+const char* PARAM_INPUT_4 = "gateway";
+
+// File paths to save input values permanently
+const char* ssidPath = "/ssid.txt";
+const char* passPath = "/pass.txt";
+const char* ipPath = "/ip.txt";
+const char* gatewayPath = "/gateway.txt";
+
+// Timer constant
+const long interval = 10000;  // interval to wait for Wi-Fi connection (milliseconds)
+
+// Set LED GPIO
+const int ledPin = LED_BUILTIN;
+
+
+
+// OLED Screen Constants
 #define SCREEN_WIDTH 128  // OLED display width, in pixels
 #define SCREEN_HEIGHT 64  // OLED display height, in pixels
 
@@ -35,24 +63,82 @@ TwoWire SENSORI2C = TwoWire(1); // second, seperate, I2C bus
 Adafruit_BME280 bme;
 
 
+
+// From WifiManager
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+//Variables to save values from HTML form
+String ssid;
+String pass;
+String ip;
+String gateway;
+
+IPAddress localIP;
+//IPAddress localIP(192, 168, 1, 200); // hardcoded
+
+// Set your Gateway IP address
+IPAddress localGateway;
+//IPAddress localGateway(192, 168, 1, 1); //hardcoded
+IPAddress subnet(255, 255, 0, 0);
+
+// Timer variables
+unsigned long previousMillis = 0;
+
+// Stores LED state
+String ledState;
+
 /*
  * ===============================================================
  * End of global declarations\
 */
 
-void setup() 
-{
-  // put your setup code here, to run once:
 
+void setup() 
+{ // put your setup code here, to run once:
   // Setup a serial port so we can send (and recieve) text to a monitoring computer.
   // This is a "virtual" serial port that is sent via USB.
   Serial.begin(9600);
   Serial.println("Starting up...");
+  
+  // Set an OUTPUT led
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
 
   // Setup the screen
+  setupScreen();
 
-  // On the Heltec ESP32 LoRa v2 the power for the display can be turned off.
+  // Setup the sensor
+  setupSensor();
+
+  // initialize file system (for retrival of wifi info)
+  initSPIFFS();
+
+  // initialize wifi connection, or get connection values if fails
+  connectWifi();
+}
+
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  
+  //display.
+  // Output a message to the programmers computer so we know the code is running
+  // Serial.println("run...");
+
+  // get and display the current temperature and humidity
+  tempAndHumidityReading();
+
+  // wait a second before doing it all again
+  delay(1000);
+}
+
+
+void setupScreen()
+{
   // Turn on the screen
+  // On the Heltec ESP32 LoRa v2 the power for the display can be turned off.
   pinMode(VEXT,OUTPUT);
 	digitalWrite(VEXT, LOW);
   // and give a slight delay for it to initialize
@@ -74,7 +160,10 @@ void setup()
   display.display();
   display.setTextSize(1);
   display.setTextColor(WHITE);
+}
 
+void setupSensor()
+{
   // setup hardware pins for use to communicate to the sensor
   pinMode(SENSOR_I2C_SCL, OUTPUT);
   digitalWrite(SENSOR_I2C_SCL, HIGH);
@@ -101,19 +190,6 @@ void setup()
   }
 }
 
-void loop() 
-{
-  // put your main code here, to run repeatedly:
-  //display.
-  // Output a message to the programmers computer so we know the code is running
-  Serial.println("run...");
-
-  // get and display the current temperature and humidity
-  tempAndHumidityReading();
-
-  // wait a second before doing it all again
-  delay(1000);
-}
 
 // and here is the code for the tempAndHumidityReading function
 void tempAndHumidityReading()
@@ -146,4 +222,239 @@ void tempAndHumidityReading()
   display.print(" %"); 
   
   display.display();  
+}
+
+
+////////////////// SPIFFS File System ////////////////////////////
+
+// Initialize SPIFFS
+void initSPIFFS() 
+{
+  if (!SPIFFS.begin(true)) 
+  {
+    Serial.println("An error has occurred while mounting SPIFFS");
+  }
+  Serial.println("SPIFFS mounted successfully");
+}
+
+// Read File from SPIFFS
+String readFile(fs::FS &fs, const char * path)
+{
+  Serial.printf("Reading file: %s\r\n", path);
+
+  File file = fs.open(path);
+  if(!file || file.isDirectory())
+  {
+    Serial.println("- failed to open file for reading");
+    return String();
+  }
+  
+  String fileContent;
+  while(file.available())
+  {
+    fileContent = file.readStringUntil('\n');
+    break;     
+  }
+  return fileContent;
+}
+
+// Write file to SPIFFS
+void writeFile(fs::FS &fs, const char * path, const char * message)
+{
+  Serial.printf("Writing file: %s\r\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if(!file)
+  {
+    Serial.println("- failed to open file for writing");
+    return;
+  }
+  if(file.print(message))
+  {
+    Serial.println("- file written");
+  } 
+  else 
+  {
+    Serial.println("- write failed");
+  }
+}
+
+
+//////////////////// WIFI ////////////////////
+
+void connectWifi()
+{
+  // Load values saved in SPIFFS
+  ssid = readFile(SPIFFS, ssidPath);
+  pass = readFile(SPIFFS, passPath);
+  ip = readFile(SPIFFS, ipPath);
+  gateway = readFile (SPIFFS, gatewayPath);
+  Serial.println(ssid);
+  Serial.println(pass);
+  Serial.println(ip);
+  Serial.println(gateway);
+
+  if (initWiFi())
+  {
+    // wifi works fine, now serve web server:
+    serveLedChangerPage();
+  }
+  else
+  {
+    // wifi does not work, create a web page to ask for wifi login:
+    serveNetworkCredentialForm();
+  }
+    
+}
+
+// Initialize WiFi
+bool initWiFi() 
+{
+  if(ssid=="" || ip=="")
+  {
+    Serial.println("Undefined SSID or IP address.");
+    return false;
+  }
+
+  WiFi.mode(WIFI_STA);
+  localIP.fromString(ip.c_str());
+  localGateway.fromString(gateway.c_str());
+
+  if (!WiFi.config(localIP, localGateway, subnet))
+  {
+    Serial.println("STA Failed to configure");
+    return false;
+  }
+  
+  WiFi.begin(ssid.c_str(), pass.c_str());
+  Serial.println("Connecting to WiFi...");
+
+  unsigned long currentMillis = millis();
+  previousMillis = currentMillis;
+
+  while(WiFi.status() != WL_CONNECTED) 
+  {
+    currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) 
+    {
+      Serial.println("Failed to connect.");
+      return false;
+    }
+  }
+
+  Serial.println(WiFi.localIP());
+  return true;
+}
+
+////////////////////// Web Forms ////////////////////////////
+
+void serveLedChangerPage()
+{ 
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) 
+  {
+    request->send(SPIFFS, "/index.html", "text/html", false, processor);
+  });
+  server.serveStatic("/", SPIFFS, "/");
+  
+  // Route to set GPIO state to HIGH
+  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request) 
+  {
+    digitalWrite(ledPin, HIGH);
+    request->send(SPIFFS, "/index.html", "text/html", false, processor);
+  });
+
+  // Route to set GPIO state to LOW
+  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request) 
+  {
+    digitalWrite(ledPin, LOW);
+    request->send(SPIFFS, "/index.html", "text/html", false, processor);
+  });
+  server.begin();
+}
+
+void serveNetworkCredentialForm()
+{ // wifi does not work, create a web page to ask for wifi login:
+  // Connect to Wi-Fi network with SSID and password
+  Serial.println("Setting AP (Access Point)");
+  // NULL sets an open Access Point
+  WiFi.softAP("ESP-WIFI-MANAGER", NULL);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP); 
+
+  // Web Server Root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/wifimanager.html", "text/html");
+  });
+  
+  server.serveStatic("/", SPIFFS, "/");
+  
+  server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) 
+  {
+    int params = request->params();
+    for(int i=0;i<params;i++){
+      AsyncWebParameter* p = request->getParam(i);
+      if(p->isPost()){
+        // HTTP POST ssid value
+        if (p->name() == PARAM_INPUT_1) {
+          ssid = p->value().c_str();
+          Serial.print("SSID set to: ");
+          Serial.println(ssid);
+          // Write file to save value
+          writeFile(SPIFFS, ssidPath, ssid.c_str());
+        }
+        // HTTP POST pass value
+        if (p->name() == PARAM_INPUT_2) {
+          pass = p->value().c_str();
+          Serial.print("Password set to: ");
+          Serial.println(pass);
+          // Write file to save value
+          writeFile(SPIFFS, passPath, pass.c_str());
+        }
+        // HTTP POST ip value
+        if (p->name() == PARAM_INPUT_3) {
+          ip = p->value().c_str();
+          Serial.print("IP Address set to: ");
+          Serial.println(ip);
+          // Write file to save value
+          writeFile(SPIFFS, ipPath, ip.c_str());
+        }
+        // HTTP POST gateway value
+        if (p->name() == PARAM_INPUT_4) {
+          gateway = p->value().c_str();
+          Serial.print("Gateway set to: ");
+          Serial.println(gateway);
+          // Write file to save value
+          writeFile(SPIFFS, gatewayPath, gateway.c_str());
+        }
+        //Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
+      }
+    }
+    request->send(200, "text/plain", "Done. ESP will restart, connect to your router and go to IP address: " + ip);
+    delay(3000);
+    ESP.restart();
+  });
+  server.begin();
+}
+
+
+// Replaces placeholder with LED state value
+String processor(const String& var) 
+{
+  if(var == "STATE") 
+  {
+    if(digitalRead(ledPin)) 
+    {
+      ledState = "ON";
+    }
+    else 
+    {
+      ledState = "OFF";
+    }
+    return ledState;
+  }
+  return String();
 }
